feb57a86ec905d708ca9b75e887c7de55ac35041
Jerrywx <23601145@qq.com>
2018-03-06 11:03:51 +0800

    获取图片

--- JRImagePicker/MainController/ViewController.m
+++ JRImagePicker/MainController/ViewController.m
@@ -14,8 +14,15 @@
 #import "JRImageListController.h"
 #import "Header.h"
 #import "JRPickerViewController.h"
+#import "JRImageCell.h"
 
-@interface ViewController () <JRImageListControllerDelegate>
+@interface ViewController () <JRImageListControllerDelegate, UICollectionViewDelegate, UICollectionViewDataSource>
+
+@property (nonatomic, strong) UICollectionView	*collectionView;
+
+@property (nonatomic, strong) UICollectionViewFlowLayout	*layout;
+
+@property (nonatomic, strong) NSArray<JRAsset *>			*imageList;
 
 @end
 
@@ -34,7 +41,33 @@
 	[selImage addTarget:self
 				 action:@selector(openPicker)
 	   forControlEvents:UIControlEventTouchUpInside];
+	
+	
+	///
+	CGRect frame = CGRectMake(0, 180, Screen_w, Screen_w);
+	self.collectionView = [[UICollectionView alloc] initWithFrame:frame
+											 collectionViewLayout:self.layout];
+	self.collectionView.delegate = self;
+	self.collectionView.dataSource = self;
+	[self.view addSubview:self.collectionView];
+	[self.collectionView registerClass:[JRImageCell class] forCellWithReuseIdentifier:@"cc"];
+	
 }
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+	return self.imageList.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+	
+	JRImageCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cc" forIndexPath:indexPath];
+	cell.backgroundColor = [UIColor orangeColor];
+	cell.asset = self.imageList[indexPath.row];
+	
+	return cell;
+}
+
+
 
 /// 打开相册
 - (void)openPicker {
@@ -51,6 +84,23 @@
 		finishPickingPhotos:(NSArray<JRAsset *> *)assets
 	ifSelectedOriginalPhoto:(BOOL)isSelectOriginalPhoto {
 	NSLog(@"======= %zd", assets.count);
+	self.imageList = assets.copy;
+	[self.collectionView reloadData];
+}
+
+- (UICollectionViewFlowLayout *)layout {
+	if (_layout) {
+		return _layout;
+	}
+	
+	_layout = [UICollectionViewFlowLayout new];
+	_layout.minimumLineSpacing = Margin_w;
+	_layout.minimumInteritemSpacing = Margin_w;
+	NSInteger count = 4;
+	CGFloat width = (Screen_w - Margin_w * 5) / count;
+	_layout.itemSize = CGSizeMake(width, width);
+	
+	return _layout;
 }
 
 @end
